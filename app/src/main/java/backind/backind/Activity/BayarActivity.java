ded06d57558b409653002546c949386b96aae877
Mindha Ningrum <mindhaningrum11@gmail.com>
2018-03-31 08:11:40 +0700

    Update Bayar Screen

--- app/src/main/java/backind/backind/Activity/BayarActivity.java
+++ app/src/main/java/backind/backind/Activity/BayarActivity.java
@@ -1,14 +1,79 @@
 package backind.backind.Activity;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import backind.backind.R;
 
 public class BayarActivity extends AppCompatActivity {
+
+    TextView txtJumlahTiket;
+    ImageButton btnMinus, btnPlus;
+    int n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bayar);
+
+        //status bar
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorWhite));
+
+        //action bar
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#ffffff"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+        getSupportActionBar().setElevation(0);
+
+        Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back_black_24dp);
+        upArrow.setColorFilter(ContextCompat.getColor(this, R.color.colorHitam), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
+        txtJumlahTiket = findViewById(R.id.jumlah);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnPlus = findViewById(R.id.btnPlus);
+
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (n > 0) {
+                    n--;
+                    txtJumlahTiket.setText(String.valueOf(n));
+                } else  {
+                    Context context = getApplicationContext();
+                    Toast toast = Toast.makeText(context, "Isi Jumlah Tiket", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (n < 5) {
+                    n++;
+                    txtJumlahTiket.setText(String.valueOf(n));
+                } else {
+                    Context context = getApplicationContext();
+                    Toast toast = Toast.makeText(context, "Maksimal pembelian tiket hanya 5", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/backind/backind/Activity/KonfirmasiActivity.java
@@ -0,0 +1,16 @@
+package backind.backind.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import backind.backind.R;
+
+
+public class KonfirmasiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konfirmasi);
+    }
+}
