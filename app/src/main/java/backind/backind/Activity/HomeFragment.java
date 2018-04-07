c82ed4a320edf360dbbd61ff19e59954bf15351b
Mindha Ningrum <mindhaningrum11@gmail.com>
2018-04-07 20:42:05 +0700

    Homeslider

--- app/src/main/java/backind/backind/Activity/HomeFragment.java
+++ app/src/main/java/backind/backind/Activity/HomeFragment.java
@@ -7,11 +7,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import backind.backind.R;
+import backind.backind._sliders.FragmentSlider;
+import backind.backind._sliders.SliderIndicator;
+import backind.backind._sliders.SliderPagerAdapter;
+import backind.backind._sliders.SliderView;
 
 
 public class HomeFragment extends Fragment {
+
+    private SliderPagerAdapter mAdapter;
+    private SliderIndicator mIndicator;
+
+    private SliderView sliderView;
+    private LinearLayout mLinearLayout;
 
 
     public HomeFragment() {
@@ -22,8 +36,26 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+
+        sliderView = (SliderView) rootView.findViewById(R.id.sliderView);
+        mLinearLayout = (LinearLayout) rootView.findViewById(R.id.pagesContainer);
+        setupSlider();
+        return rootView;
+    }
+
+    private void setupSlider() {
+        sliderView.setDurationScroll(800);
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(FragmentSlider.newInstance(String.valueOf(getResources().getDrawable(R.drawable.img_slider_1))));
+        fragments.add(FragmentSlider.newInstance(String.valueOf(getResources().getDrawable(R.drawable.img_slider_2))));
+        fragments.add(FragmentSlider.newInstance(String.valueOf(getResources().getDrawable(R.drawable.img_slider_3))));
+
+        mAdapter = new SliderPagerAdapter(getFragmentManager(), fragments);
+        sliderView.setAdapter(mAdapter);
+        mIndicator = new SliderIndicator(getActivity(), mLinearLayout, sliderView, R.drawable.indicator_circle);
+        mIndicator.setPageCount(fragments.size());
+        mIndicator.show();
     }
 
 
