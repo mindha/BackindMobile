b4846ed2e9af378c51ac44ee322932877e573909
Mindha Ningrum <mindhaningrum11@gmail.com>
2018-04-26 23:02:41 +0700

    Clean UI design

--- app/src/main/java/backind/backind/Activity/RegisterActivity.java
+++ app/src/main/java/backind/backind/Activity/RegisterActivity.java
@@ -44,7 +44,16 @@ public class RegisterActivity extends AppCompatActivity {
         Window window = this.getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorWhite));
 
+        //action bar
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#ffffff"));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+        getSupportActionBar().setElevation(0);
+
+        Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.ic_arrow_back_black_24dp);
+        upArrow.setColorFilter(ContextCompat.getColor(this, R.color.colorHitam), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
         Spinner spinner = (Spinner) findViewById(R.id.jenis_kelamin);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
